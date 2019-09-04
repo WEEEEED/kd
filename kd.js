ef54d854c4bb8407f29e7bb0c43fa3b5ee012c3e
WEEEEED <51104222+WEEEEED@users.noreply.github.com>
2019-09-04 06:22:18 +0300

    Add files via upload

---
+++ kd.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+    if(message.content == (prefix + "حالة")) {
+    const embed = new Discord.RichEmbed()
+    .setDescription(`**Members info🔋
+:green_heart: online:   ${message.guild.members.filter(m=>m.presence.status == 'online').size}
+:heart:dnd:       ${message.guild.members.filter(m=>m.presence.status == 'dnd').size}
+:yellow_heart: idle:      ${message.guild.members.filter(m=>m.presence.status == 'idle').size}   
+:black_heart: offline:   ${message.guild.members.filter(m=>m.presence.status == 'offline').size} 
+:blue_heart:   all:  ${message.guild.memberCount}**`)         
+         message.channel.send({embed});
+
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login('NjE4NjQxMTY0NjA1ODQ5NjA2.XW8oig.WOSGsp0iDJhfh4zIYw1-e-GKXzo');
---
+++ start.bat
@@ -0,0 +1,2 @@
+node kd.js
+pasue
